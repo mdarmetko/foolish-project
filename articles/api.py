443c313eaab9efb945f7176128be331463353315
Mike Darmetko <mdarmetko@gmail.com>
2019-10-29 10:31:52 -0600

    fix for data file encoding

--- articles/api.py
+++ articles/api.py
@@ -8,6 +8,6 @@ def get_quotes():
   return read_json_from_file('quotes_api.json')
 
 def read_json_from_file(filename):
-  with open(filename) as file:
+  with open(filename, encoding='utf8') as file:
     s = file.read()
   return json.loads(s)
